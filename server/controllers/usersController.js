4029cf897c30066f6ca41f2e3c900db2614b577e
Azul Heynen <azulheynen@gmail.com>
2024-09-09 18:54:04 -0300

    fix notes controller

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -7,10 +7,8 @@ const bcrypt = require("bcrypt");
 // @route GET /users
 // @access Private
 const getAllUsers = asyncHandler(async (req, res) => {
-  // Get all users from MongoDB
   const users = await User.find().select("-password").lean();
 
-  // If no users
   if (!users?.length) {
     return res.status(400).json({ message: "No users found" });
   }
