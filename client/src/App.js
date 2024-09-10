f98e8d6bd4959639c0a82aa5c4b3c5b6b22bdb2f
Azul Heynen <azulheynen@gmail.com>
2024-09-10 15:23:13 -0300

    add routes for new note and user

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,11 @@ import { Routes, Route } from "react-router-dom";
 import Welcome from "./features/auth/Welcome";
 import NotesList from "./features/notes/NoteList";
 import UsersList from "./features/users/UsersList";
+import EditUser from "./features/users/EditUser";
+import NewUserForm from "./features/users/NewUserForm";
+import EditNote from "./features/notes/EditNote";
+import NewNote from "./features/notes/NewNote";
+
 function App() {
   return (
     <Routes>
@@ -17,12 +22,16 @@ function App() {
         <Route path="dash" element={<DashLayout />}>
           <Route index element={<Welcome />} />
 
-          <Route path="notes">
-            <Route index element={<NotesList />} />
-          </Route>
-
           <Route path="users">
             <Route index element={<UsersList />} />
+            <Route path=":id" element={<EditUser />}></Route>
+            <Route path="new" element={<NewUserForm />}></Route>
+          </Route>
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+            <Route path=":id" element={<EditNote />}></Route>
+            <Route path="new" element={<NewNote />}></Route>
           </Route>
         </Route>
         {/* End Dash */}
