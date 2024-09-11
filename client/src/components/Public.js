e267b0c339faf25a16fc5fd77ca076c31f093479
Azul Heynen <azulheynen@gmail.com>
2024-09-11 13:11:40 -0300

    css welcome page

--- client/src/components/Public.js
+++ client/src/components/Public.js
@@ -1,37 +1,49 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Public = () => {
+  useEffect(() => {
+    // You can perform any additional effects when the component mounts, if needed.
+  }, []);
+
   const content = (
-    <section className="public">
-      <header>
-        <h1>
-          Welcome to <span className="nowrap"> Artful Alchemists Central!</span>
-        </h1>
-      </header>
-      <main className="public__main">
-        <p>
-          Situated in the creative heartbeat of Buenos Aires, Artful Alchemists
-          is your ultimate hub for innovative design solutions and user-centric
-          experiences. Our talented team of graphic designers and UI/UX experts
-          is here to transform your visions into visually stunning realities.
-        </p>
-        <address className="public__addr">
-          Design Central
+    <div className="public-container">
+      <section className="poster">
+        <header className="poster-header">
+          <h1>
+            Welcome to{" "}
+            <span className="nowrap">Artful Alchemists Central!</span>
+          </h1>
+        </header>
+        <main className="public__main">
+          <p>
+            Situated in the creative heartbeat of Buenos Aires, Artful
+            Alchemists is your ultimate hub for innovative design solutions and
+            user-centric experiences. Our talented team of graphic designers and
+            UI/UX experts is here to transform your visions into visually
+            stunning realities.
+          </p>
+          <address className="public__addr">
+            Design Central
+            <br />
+            5678 Visionary Blvd
+            <br />
+            Buenos Aires, CF, 1280
+            <br />
+            <a href="tel:+[000-800-999]">Connect with us!</a>
+          </address>
           <br />
-          5678 Visionary Blvd
-          <br />
-          Buenos Aires, CF, 1280
-          <br />
-          <a href="tel:+[000-800-999]">Connect with us: [Phone Number]</a>
-        </address>
-        <br />
-        <p>Curated by: All</p>
-      </main>
-      <footer>
-        <Link to="/dash">Enter the Designer's Notes Studio</Link>
-      </footer>
-    </section>
+          <p>Curated by: All</p>
+        </main>
+        <footer>
+          <Link className="welcome-link" to="/dash">
+            Enter the Designer's Notes Studio
+          </Link>
+        </footer>
+      </section>
+    </div>
   );
+
   return content;
 };
 
--- client/src/features/auth/Welcome.js
+++ client/src/features/auth/Welcome.js
@@ -1,41 +1,53 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Public from "../../components/Public";
 
 const Welcome = () => {
+  const [isBlurred, setIsBlurred] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsBlurred(true);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   const date = new Date();
   const today = new Intl.DateTimeFormat("en-US", {
     dateStyle: "full",
     timeStyle: "long",
   }).format(date);
 
-  const content = (
-    <section className="welcome">
-      <p>{today}</p>
-
-      <h1 class="rainbow-text">Welcome!</h1>
-      <p>
-        <Link className="letters" to="/dash/notes">
-          View techNotes
-        </Link>
-      </p>
-      <p>
-        <Link className="letters" to="/dash/notes/new">
-          Add new note
-        </Link>
-      </p>
-
-      <p>
-        <Link className="letters" to="/dash/users">
-          View User Settings
-        </Link>
-      </p>
-      <p>
-        <Link className="letters" to="/dash/users/new">
-          Add new User
-        </Link>
-      </p>
-    </section>
+  return (
+    <div className="welcome-container">
+      <div className={`background-image ${isBlurred ? "blurred" : ""}`}></div>
+      <section className="welcome">
+        <p>{today}</p>
+        <h1 className="rainbow-text">Welcome!</h1>
+        <p>
+          <Link className="letters" to="/dash/notes">
+            View techNotes
+          </Link>
+        </p>
+        <p>
+          <Link className="letters" to="/dash/notes/new">
+            Add new note
+          </Link>
+        </p>
+        <p>
+          <Link className="letters" to="/dash/users">
+            View User Settings
+          </Link>
+        </p>
+        <p>
+          <Link className="letters" to="/dash/users/new">
+            Add new User
+          </Link>
+        </p>
+      </section>
+    </div>
   );
-
-  return content;
 };
+
 export default Welcome;
